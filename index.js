51dd69786134600207d7e5ecaa39d5a5a1479f40
digijin <james@digijin.com>
2016-10-21 14:03:35 +1100

    cleanup

--- index.js
+++ index.js
@@ -4,23 +4,23 @@ import words from './data/five.json'
 
 
 
-// let gen = new Gen(words, 5, 5);
-
-let testwords = [
-  " mit ",
-  "zebra",
-  "idiom",
-  "gizmo",
-  " cap ",
-  " zig ",
-  "medic",
-  "ibiza",
-  "tromp",
-  " amo "
-]
-
-let gen = new Gen(testwords)
+let gen = new Gen(words, 5, 5);
 //
+// let testwords = [
+//   " mit ",
+//   "zebra",
+//   "idiom",
+//   "gizmo",
+//   " cap ",
+//   " zig ",
+//   "medic",
+//   "ibiza",
+//   "tromp",
+//   " amo "
+// ]
+//
+// let gen = new Gen(testwords)
+// //
 // let across = [" mit ", "zebra", "idiom"];
 // let down = [" zig ", "medic"]
 //
