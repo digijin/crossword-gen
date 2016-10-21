56b23584a55f9ef2648639942cb9b28f9e7a9d32
digijin <james@digijin.com>
2016-10-21 15:41:17 +1100

    four too

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 
 import Gen from './lib/gen';
-import words from './data/five.json'
+import five from './data/five.json'
+import four from './data/four.json'
 import fs from 'fs';
 
 
-let gen = new Gen(words, 5);
+// let gen = new Gen(five, 5);
+let gen = new Gen(four, 4);
 
 
 gen.generate();
+console.log(gen.results.length, 'results');
 fs.writeFileSync('output.json', JSON.stringify(gen.results, null, 2));
